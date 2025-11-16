cc7214e598c9d620715bf7037bad01e5e037f2b6
NikiniEkanayaka <nikiniekanayaka00@gmail.com>
2025-11-16 09:59:53 +0530

    Data Ingestion Config in config_entity

---
+++ KLEOS_Recommender/entity/config_entity.py
@@ -0,0 +1,52 @@
+from collections import namedtuple
+
+# ---------------------------------------------------------
+# DATA INGESTION CONFIG
+# ---------------------------------------------------------
+DataIngestionConfig = namedtuple("DataIngestionConfig", [
+    "dataset_download_url",
+    "raw_data_dir",
+    "ingested_dir"
+])
+
+
+# # ---------------------------------------------------------
+# DATA VALIDATION CONFIG
+# # ---------------------------------------------------------
+# DataValidationConfig = namedtuple("DataValidationConfig", [
+#     "clean_data_dir",
+#     "books_csv_file",
+#     "ratings_csv_file",
+#     "serialized_objects_dir"
+# ])
+
+
+# # ---------------------------------------------------------
+# # DATA TRANSFORMATION CONFIG
+# # ---------------------------------------------------------
+# DataTransformationConfig = namedtuple("DataTransformationConfig", [
+#     "clean_data_file_path",
+#     "transformed_data_dir"
+# ])
+
+
+# # ---------------------------------------------------------
+# # MODEL TRAINER CONFIG
+# # ---------------------------------------------------------
+# ModelTrainerConfig = namedtuple("ModelTrainerConfig", [
+#     "transformed_data_file_path",
+#     "trained_model_dir",
+#     "trained_model_name"
+# ])
+
+
+# # ---------------------------------------------------------
+# # MODEL RECOMMENDATION CONFIG
+# # ---------------------------------------------------------
+# ModelRecommendationConfig = namedtuple("ModelRecommendationConfig", [
+#     "book_name_serialized_objects",
+#     "book_pivot_serialized_objects",
+#     "final_rating_serialized_objects",
+#     "trained_model_path",
+#     "poster_api_url"
+# ])
